3b7863a7f94b770ae5db502716437f1a294c8ac1
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-16 17:59:27 +0700

    Update AstralHub.lua

--- AstralHub.lua
+++ AstralHub.lua
@@ -7001,7 +7001,7 @@ walkWater:OnChanged(function(Value)
   end
 end)
 
-iceWalk = Tabs.Misc:AddToggle("iceWalk", {Title = "Đi Trên Băng", Description = "Đi y hệt như đi trên nước nhưng có hiệu ứng trái băng-v2", Default = false})
+iceWalk = Tabs.Misc:AddToggle("iceWalk", {Title = "Đi Trên Băng", Description = "Như đi trên nước nhưng có hiệu ứng đi của trái băng-v2", Default = false})
 iceWalk:OnChanged(function(Value)
   _G.WalkWater = Value
 end)
