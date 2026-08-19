7df1f90343cbe7b60ed7306fec6bb3b13350732a
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-19 16:40:33 +0700

    Update Mingaminghub.lua

--- Mingaminghub.lua
+++ Mingaminghub.lua
@@ -319,7 +319,7 @@ local v15 = v14:CreateWindow({
     Theme = "Light",
     Acrylic = false,
     Size = UDim2.fromOffset(500, 320),
-    --MinimizeKey = Enum.KeyCode.End
+    MinimizeKey = Enum.KeyCode.End
 });
 local v16 = {
     Home = v15:AddTab({
