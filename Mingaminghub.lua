44afca885fb78640c31a6891b679234a8fba7e74
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-19 22:30:53 +0700

    Update Mingaminghub.lua

--- Mingaminghub.lua
+++ Mingaminghub.lua
@@ -3567,9 +3567,9 @@ task.spawn(function()
                 end
             end
             if isAlive then
-                BossStatus:SetDesc(bossName .. "  Đang Sống")
+                BossStatus:SetDesc(bossName .. "  (🟢 Đang Sống)")
             else
-                BossStatus:SetDesc(bossName .. "  (Chờ hồi sinh)")
+                BossStatus:SetDesc(bossName .. "  (🔴 Chờ hồi sinh)")
             end
         end)
     end
