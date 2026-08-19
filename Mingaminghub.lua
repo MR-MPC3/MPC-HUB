d09644d54d37f8883a66ba79472b1caf7bb2dd50
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-19 23:11:41 +0700

    Update Mingaminghub.lua

--- Mingaminghub.lua
+++ Mingaminghub.lua
@@ -2421,7 +2421,7 @@ function BTPZ(v209)
     task.wait();
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v209;
 end
-TweenSpeed = 300
+TweenSpeed = 250
 local CurrentTween = nil
 function Tween(targetCFrame)
     if not game.Players.LocalPlayer.Character or not game.Players.LocalPlayer.Character:FindFirstChild("HumanoidRootPart") then
