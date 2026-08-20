0ba87a3acdb221708813f08bc19cac24372bbaac
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-20 11:43:18 +0700

    Update Mingaminghub.lua

--- Mingaminghub.lua
+++ Mingaminghub.lua
@@ -2403,7 +2403,7 @@ function BTPZ(v209)
     task.wait();
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = v209;
 end
-local TweenSpeed = 280
+local TweenSpeed = 300
 local CurrentTween = nil
 _G.StopTween = false
 function Tween(targetCFrame)
