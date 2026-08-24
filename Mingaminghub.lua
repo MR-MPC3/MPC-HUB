5e118fec2535e4f719283bbc86c174b51b691581
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-08-24 21:57:13 +0700

    Update Mingaminghub.lua

--- Mingaminghub.lua
+++ Mingaminghub.lua
@@ -398,8 +398,8 @@ local TabDefinitions = {
     { Key = "Player",   Title = "Người Chơi",  Icon = "user" },
     { Key = "Teleport", Title = "Dịch Chuyển", Icon = "map-pin" },
     { Key = "Fruit",    Title = "Trái",        Icon = "apple" },
-    { Key = "Raid",     Title = "Tập Kích",    Icon = "shield-alert" },
-    { Key = "Race",     Title = "Tộc",         Icon = "dna" },
+    { Key = "Raid",     Title = "Tập Kích",    Icon = "swords" },
+    { Key = "Race",     Title = "Tộc",         Icon = "zap" },
     { Key = "Shop",     Title = "Cửa Hàng",    Icon = "shopping-cart" },
     { Key = "Misc",     Title = "Khác",        Icon = "layers" }
 }
