8d28c8c0d1c1d82c47e52ae09dd5b9d73fcb8b48
MR-MPC3 <nguyendangmigiang@gmail.com>
2026-09-05 18:40:11 +0700

    Update FatCatHub.lua

--- FatCatHub.lua
+++ FatCatHub.lua
@@ -152,7 +152,7 @@ local Window = Fluent:CreateWindow({
     TabWidth = 160, 
     Theme = "Dark",
     Acrylic = false,
-    Size = UDim2.fromOffset(520, 280),
+    Size = UDim2.fromOffset(510, 290),
     --logo nút ẩn hiện menu chính
     ToggleIcon = "rbxassetid://13717478897",
 })
